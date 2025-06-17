62fb8fc9c3c8b7af3480c5d4a39b09ac779115c0
elias3446 <59986780+elias3446@users.noreply.github.com>
2025-06-17 15:10:02 -0500

    Summary

--- src/components/asistent/IntelligentChat.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import NewJarvisChat from './NewJarvisChat';
-
-const IntelligentChat: React.FC = () => {
-  return <NewJarvisChat />;
-};
-
-export default IntelligentChat;
